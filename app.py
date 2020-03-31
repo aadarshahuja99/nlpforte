2e8585f890d1e3f5b56b3597f8f9aab51de1dbfa
Aadarsh Ahuja <aadarshahuja99@gmail.com>
2020-03-31 15:46:32 +0530

    changed

--- app.py
+++ app.py
@@ -23,11 +23,15 @@ def predict():
 		vect = cv.transform(data).toarray()
 		my_prediction = clf.predict(vect)
 	return render_template('result.html',prediction = my_prediction)
-@app.route('/predict_api/<string:review>',methods=['GET'])
-def predict_api(review):
+@app.route('/predict_api/')
+def predict_api():
+    review=request.args['rev']
     vect=cv.transform(review).toarray()
     my_prediction = clf.predict(vect)
     return jsonify({"prediction":my_prediction})
+
+#api.add_resource(home,'/')
+#api.add_resource(predict_api,'/predict_api/<string:review>')
 
 if __name__ == "__main__":
     app.run(debug=True)
